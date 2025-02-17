210a5b8e360ce0557f1455edf821ff7006685a6b
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-02-17 23:43:11 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.96%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n= nums.size();
-        int low = 0, high = n-1;
-        while(low<=high){
+        // Approach -1 // Linear Search
+            // Iterating array 
+            // find tagret in the array return index or else return -1 if not found
+
+
+        //Approach -2 // Binary Search
+          int low =0,high = nums.size()-1;
+          while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]<target){
-                low = mid+1;
-            }else{
+            }else if(nums[mid]>target){
                 high = mid-1;
-            }
-        }
-        return -1;
+            }else{
+                low = mid+1;
+            }            
+          }
+          return -1;
+
     }
 };
