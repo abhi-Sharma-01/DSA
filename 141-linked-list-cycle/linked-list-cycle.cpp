c5607876bb52b20022f89c9bfe8a0fe9a92ae22f
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-03-04 16:30:35 +0530

    Time: 11 ms (41.83%) | Memory: 15.1 MB (7.74%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,25 +9,35 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
-        if(head == nullptr || head->next == nullptr){
-            return false;
-        }
-        //try using slow and faster pointer if circulr then they sholud collide at same point 
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(slow!=fast){
-            if(fast->next == nullptr || fast->next->next==nullptr){
-                return false;
+        // Method 1:
+        // if(head == nullptr || head->next == nullptr){
+        //     return false;
+        // }
+        // //try using slow and faster pointer if circulr then they sholud collide at same point 
+        // ListNode* slow = head;
+        // ListNode* fast = head->next;
+        // while(slow!=fast){
+        //     if(fast->next == nullptr || fast->next->next==nullptr){
+        //         return false;
+        //     }
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        // }
+        // return true;
+
+        //Method 2:
+
+        ListNode* temp = head;
+        map<ListNode*, int> mapp;
+        while(temp != NULL){
+            if(mapp[temp]!= 0){
+                return true;
             }
-            slow = slow->next;
-            fast = fast->next->next;
+            mapp[temp]++;
+            temp = temp->next;
+            
         }
-        return true;
-
-
-
-        
+        return false;
         
     }
 };
