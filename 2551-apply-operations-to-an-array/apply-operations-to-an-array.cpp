0a98e99b7a8e522d7f1567e69e3b60d554fbac4a
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-03-22 16:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (61.14%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
+        // Method 2
         int n = nums.size();
-        for(int i =0;i<n-1;i++){
+         for(int i =0;i<n-1;i++){
             if(nums[i]==nums[i+1]){
                 nums[i]=2*nums[i];
                 nums[i+1]=0;
             }
-            continue;
         }
-        int left =0, right = n-1;
-        while(left<right){
-            if(nums[left]!=0){
-                left++;
-            }
-            if(nums[left]==0){
-                for(int i=left;i<n-1;i++){
-                    nums[i]=nums[i+1];
-                }
-                nums[right]=0;
-                right--;
+        vector<int> ans(n, 0);// initalise all element by 0 taaki we need not to enter in last
+        int pos = 0; 
+        for (int num : nums) {
+            if (num != 0) {
+                ans[pos++] = num; 
             }
         }
-        return nums;
+        return ans;
+
+        //Method 1
+        // int n = nums.size();
+        // for(int i =0;i<n-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         nums[i]=2*nums[i];
+        //         nums[i+1]=0;
+        //     }
+        //     continue;
+        // }
+        // int left =0, right = n-1;
+        // while(left<right){
+        //     if(nums[left]!=0){
+        //         left++;
+        //     }
+        //     if(nums[left]==0){
+        //         for(int i=left;i<n-1;i++){
+        //             nums[i]=nums[i+1];
+        //         }
+        //         nums[right]=0;
+        //         right--;
+        //     }
+        // }
+        // return nums;
     }
 };
