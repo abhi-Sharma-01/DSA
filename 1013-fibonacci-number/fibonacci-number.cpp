f6f6aa7160a13893e6038df1e961d86538e2593a
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-04-29 03:04:11 +0530

    Time: 2 ms (76.27%) | Memory: 7.8 MB (37.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
+        // // M1:
+        // if(n<=1) return n;
+        // return fib(n-1) + fib(n-2);        
+
+        // M2;
         if(n<=1) return n;
-        return fib(n-1) + fib(n-2);        
+        int fib0 = 0,fib1 =1;
+        int fibn =0;
+        for(int i=2;i<=n;i++){
+            fibn = fib0 + fib1;
+            fib0 = fib1;
+            fib1 = fibn;
+        }
+        return fibn;
     }
 };
