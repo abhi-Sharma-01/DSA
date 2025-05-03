3a55482b44460390fc46d94bbad3093597b16efe
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-05-03 23:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.36%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,8 +1,28 @@
 class Solution {
 public:
+    string deciTobinary(int n){
+        if(n==0) return "0";
+        string str = "";
+        while(n > 0){
+            str += (n % 2 == 0) ? '0' : '1';
+            n /= 2;
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
     int hammingWeight(int n) {
-        // M1- using STL 
-        int cnt = __builtin_popcount(n);
+        // // M1- using STL 
+        // int cnt = __builtin_popcount(n);
+        // return cnt;
+
+        //M2 - without STL
+        string s = deciTobinary(n);
+        int cnt=0;
+        for(int i=0;i<=s.size()-1;i++){
+            if(s[i]=='1')
+                cnt++;
+        }
+
         return cnt;
 
     }
