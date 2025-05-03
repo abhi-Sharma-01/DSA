3c251d7a6fa8369d630a691c1f9e1e69c5e066bf
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-05-03 13:25:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.38%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    string cnvrtToBinary(int n){
-        if(n == 0) return "0";
-        string binary = "";
-        while(n>0){
-            binary += (n%2==0)? '0':'1';  
-            n /=2;
-        }
-        reverse(binary.begin(),binary.end());   
-        return binary;
-    }
+    // string cnvrtToBinary(int n){
+    //     if(n == 0) return "0";
+    //     string binary = "";
+    //     while(n>0){
+    //         binary += (n%2==0)? '0':'1';  
+    //         n /=2;
+    //     }
+    //     reverse(binary.begin(),binary.end());   
+    //     return binary;
+    // }
     bool isPowerOfTwo(int n) {
-        // M-1 Converting n into binary
-        string s = cnvrtToBinary(n);
-        int cnt =0;
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]=='1'){
-                cnt++;
-            }
-            if(cnt>1){
-                return false;
-            }
-        }
-        if(cnt !=1) return false;
+        // // M-1 Converting n into binary
+        // string s = cnvrtToBinary(n);
+        // int cnt =0;
+        // for(int i=s.size()-1;i>=0;i--){
+        //     if(s[i]=='1'){
+        //         cnt++;
+        //     }
+        //     if(cnt>1){
+        //         return false;
+        //     }
+        // }
+        // if(cnt !=1) return false;
+        // return true;
+
+
+        // M-2  Directly using STL
         
-        return true;
+        if(n <= 0) return false;
+        return __builtin_popcount(n) == 1;
+
+
     }
 };
