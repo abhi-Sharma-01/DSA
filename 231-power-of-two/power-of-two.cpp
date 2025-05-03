f526e1efef007fb486c859c2d7f314fc78bbd34f
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-05-03 13:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (18.64%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string cnvrtToBinary(int n){
+        if(n == 0) return "0";
+        string binary = "";
+        while(n>0){
+            binary += (n%2==0)? '0':'1';  
+            n /=2;
+        }
+        reverse(binary.begin(),binary.end());   
+        return binary;
+    }
+    bool isPowerOfTwo(int n) {
+        // M-1 Converting n into binary
+        string s = cnvrtToBinary(n);
+        int cnt =0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='1'){
+                cnt++;
+            }
+            if(cnt>1){
+                return false;
+            }
+        }
+        if(cnt !=1) return false;
+        
+        return true;
+    }
+};
