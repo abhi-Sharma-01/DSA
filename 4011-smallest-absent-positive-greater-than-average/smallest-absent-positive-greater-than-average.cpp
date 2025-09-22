a194dc105318405ce84f9fa89034a99fb51bd6a3
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-09-22 23:43:28 +0530

    Time: 22 ms (42.86%) | Memory: 92.7 MB (22.51%) - LeetSync

--- 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.cpp
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int smallestAbsent(vector<int>& nums) {
-        int sum=0;
+        int n = nums.size();
+        int sum =0;
         for(int x:nums){
-            sum+=x;
+            sum+= x;
         }
-        long long avg = (float)sum/nums.size();// let avg = 1.5 so we need to find 2
-        // let increase it 
-        avg++;// now find just smaller then it  
-        
-        unordered_set<int>s(nums.begin(),nums.end());
-        if(avg<=0) avg = 1;
-        while(1){
-            if(s.count(avg)==0) return avg;
-            avg++;
+        long long avg = (float)sum/n; //  note yaha par decimal value ka floor ho jata hai so we need to inrease the val which we need to find   real avg == 1.5 but stored as 1only
+        // therefore we need to increase by one 
+        // cout<<avg<<endl;
+        // Note yaha hame agar avg decimal mila hai i.e 
+        avg++;
+        // cout<<avg<<endl;
+        set<int> s(nums.begin(),nums.end());// uniquely store kare ga  Yes, TC - O(logN)
+
+        if(avg<=0)  avg =1;// as we need to return samllest absent positive & greater than avg
+        while(true){
+            if(s.find(avg) == s.end()) return avg; // agar element .end() tak nahi mile then thats's our answer
+            avg++; 
         }
+
         return -1;
+
+        
     }
 };
