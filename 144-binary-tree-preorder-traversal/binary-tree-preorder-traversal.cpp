79bcb575b0d02e28114a3367c7566c583977f4c1
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-09-24 14:29:12 +0530

    Time: 2 ms (4.74%) | Memory: 11 MB (41.36%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,28 +11,42 @@
  */
 class Solution {
 public:
+    void preorder(TreeNode* root,vector<int> &pre){
+        if(root==NULL){
+            return ;
+        }
+        pre.push_back(root->val);
+        preorder(root->left,pre);
+        preorder(root->right,pre);
+    }
     vector<int> preorderTraversal(TreeNode* root) {
         // Method 1 using the recursion 
-        // Method 2 using the stack
-        vector<int> preorder;
-        if(root==NULL) return preorder;
+        vector<int> pre ;
+        if(root==NULL) return pre;
+
+        preorder(root,pre);
+        return pre;
+
+        // // Method 2 using the stack
+        // vector<int> preorder;
+        // if(root==NULL) return preorder;
                 
-        stack<TreeNode*> st;
-        st.push(root);
+        // stack<TreeNode*> st;
+        // st.push(root);
 
-        while(!st.empty()){
-            root = st.top();
-            st.pop();
-            preorder.push_back(root->val);
-            if(root->right){
-                st.push(root->right);
-            }
-            if(root->left){       // Note to keep left child above on the stack we need to write it below right 
-                st.push(root->left);        
-            }
+        // while(!st.empty()){
+        //     root = st.top();
+        //     st.pop();
+        //     preorder.push_back(root->val);
+        //     if(root->right){
+        //         st.push(root->right);
+        //     }
+        //     if(root->left){       // Note to keep left child above on the stack we need to write it below right 
+        //         st.push(root->left);        
+        //     }
 
-        }
-        return preorder;
+        // }
+        // return preorder;
         
     }
 };
