8429036bf8fe854c9b9ddb2eff1ac06aada7d033
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-09-24 15:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (75.49%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,36 +11,42 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        // Method 1 : USing Recursion
+        int maxDepth(TreeNode* root){
+            if(root==NULL) return 0;
+            int leftheight = maxDepth(root->left);
+            int rightheight = maxDepth(root->right);
 
-
-        // Method 2: Using the level order where we can use counter
-        int height =0;
-        // vector<vector<int> > res;
-        if(root== NULL) return height;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            int size= q.size();
-            height++;
-            for(int i=0;i<size;i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-
-            } 
-            
+            return 1 + max(leftheight , rightheight);
         }
-
-        return height;
-
-    }
 };
+//     int maxDepth(TreeNode* root) {
+//    
+//         // Method 2: Using the level order where we can use counter
+//         int height =0;
+//         // vector<vector<int> > res;
+//         if(root== NULL) return height;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while(!q.empty()){
+//             int size= q.size();
+//             height++;
+//             for(int i=0;i<size;i++){
+//                 TreeNode* node = q.front();
+//                 q.pop();
+//                 if(node->left){
+//                     q.push(node->left);
+//                 }
+//                 if(node->right){
+//                     q.push(node->right);
+//                 }
+
+//             } 
+            
+//         }
+
+//         return height;
+
+//     }
+// };
