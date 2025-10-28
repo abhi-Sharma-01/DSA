2d32deb4212bf3b5c2bdb074204d0ed4ea5cee58
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-10-29 00:43:59 +0530

    Time: 0 ms (100.00%) | Memory: 100.6 MB (35.13%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int v = edges.size() + 1; // number of nodes
+        int res = -1;
+        vector<int> vis(v + 1, 0); // 1-based indexing
+
+        for (int i = 0; i < edges.size(); i++) { 
+            int a = edges[i][0];
+            int b = edges[i][1];
+            vis[a]++;
+            vis[b]++;
+        }
+
+        for (int i = 1; i <= v; i++) {
+            if (vis[i] == edges.size()) { 
+                res = i;
+                break;
+            }
+        }
+
+        return res;
+
+
+        // note her we need to just check any two edges as one element always gonna occur again and again
+        // if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+        //     return edges[0][0];
+        // return edges[0][1];
+
+    }
+};
