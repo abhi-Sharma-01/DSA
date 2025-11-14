edd2126038248af2c220ce95b897c0432f48234e
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-14 16:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (37.66%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool check(int start,vector<vector<int>> &graph, vector<int> &color){
+    bool check (int start, vector<vector<int>>&graph,vector<int> &color){
         queue<int> q;
-        color[start] = 0;
         q.push(start);
+        color[start] = 0;
         while(!q.empty()){
             int node = q.front();
             q.pop();
@@ -12,22 +12,22 @@ public:
                 if(color[it]== -1){
                     color[it] = !color[node];
                     q.push(it);
-                }
-                else if(color[it]== color[node]){
+                }else if (color[it]== color[node]){
                     return false;
                 }
             }
         }
         return true;
+
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int V = graph.size();
 
-        vector<int> color(V,-1);
+        int V= graph.size();        
+        vector<int> color(V+1, -1);
 
-        for(int i=0;i<V;i++){
+        for(int i=0;i<V;i++){  // we can directly do checking in this function but to chekc the connected component we need to check for each component 
             if(color[i]== -1){
-                if(!check(i,graph,color)){
+                if(check(i,graph,color)== false){
                     return false;
                 }
             }
