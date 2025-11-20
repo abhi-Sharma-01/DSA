98d96351fd23f235ffc8a6751a4a0df69b3d42fe
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-20 17:40:02 +0530

    Time: 27 ms (46.78%) | Memory: 59.3 MB (40.96%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    string invert(string s){
+    string inverse(string s){
         for(auto &c:s){  // pass by reference to edit in original string
-            c = (c=='0')? '1':'0';
+            c = ((c-'0') & 1)? '0':'1';
         }
         return s;
     }
-    char findKthBit(int n, int k) {  // 1-Based indexed
+    char findKthBit(int n, int k) {  
         string s = "0"; // for i=1
         if(n==1) return '0';
         string toadd ="";
         for(int i=2;i<=n;i++ ){
-            string invrt = invert(s);
+            string invrt = inverse(s);
             reverse(invrt.begin(),invrt.end());
             s = s + "1" + invrt;
         }
-        return s[k - 1];  // k is 1-based index, so subtract 1
+        return s[k - 1];  
 
     }
 };
