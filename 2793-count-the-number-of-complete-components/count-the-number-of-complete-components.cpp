21d7906324475f2f0625b5fcfbdf33d850c18056
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-24 16:15:57 +0530

    Time: 78 ms (25.68%) | Memory: 137.6 MB (31.50%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,52 +1,42 @@
 class Solution {
-public:                   
- // In this problem we not only need to find the number of components  
- // Here We need to find if the connected component is complete or not 
-
-
- // To check the complete components we need to store the component in vector
- // And need to check if the number of edges = size* (size-1)/ 2;
-
-    void traverse(int node,vector<vector<int>> &adjList,vector<bool> &vis, vector<int> & curr_compo){
-        vis[node] = true;
-        curr_compo.push_back(node);
-        for(int neigh : adjList[node]){
-            if(!vis[neigh]){
-                traverse(neigh,adjList,vis,curr_compo);
-            }
+public:
+    void helper(int node,vector<vector<int>> &adjList,vector<int> &Vis , vector<int>& curr){
+        Vis[node] = 1;
+        curr.push_back(node);
+        for(auto neigh:adjList[node]){
+            if(!Vis[neigh]) helper(neigh,adjList,Vis,curr);
         }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-
         vector<vector<int>> adjList(n);
-        for(int i=0;i<edges.size();i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
+        for(auto edge:edges){
+            int u = edge[0];
+            int v = edge[1];
             adjList[u].push_back(v);
             adjList[v].push_back(u);
         }
 
-        vector<bool> vis(n,false);
-        // vector<vector<int>> components;
-        int cnt =0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                vector<int>curr_compo;
-                traverse(i,adjList,vis,curr_compo);
-            
-                int edgecnt = 0;
-                for(int node : curr_compo){ // we need to check all the edges for each node in curr_compo component
-                    edgecnt += adjList[node].size();
+        vector<int> Vis(n,0);
+        int count =0;
+        for(int i =0;i<n;i++){
+            if(!Vis[i]){
+                vector<int> curr;
+                helper(i,adjList,Vis,curr);
+                
+                // to be a complete componemnt it should have a edgecount  = size*(size-1)/2
+
+                int edgeCount = 0;
+                for(auto node: curr){
+                    edgeCount += adjList[node].size();
                 }
-                edgecnt /=2;  // as we are solving for the undirected graph so we have counted double
-                // As we have curr_compo compo so to be a complete compo we need the the total edge = (size*(size-1))/2
-                int size = curr_compo.size();
-                if(edgecnt == (size*(size-1))/2){
-                    cnt++;
+                int sz = curr.size();
+
+                if(edgeCount == (sz*(sz-1))){
+                    count++;
                 }
+
             }
         }
-        return cnt;
-        
+        return count;   
     }
 };
