851fade5dd852a4e5cec172ba599f976946b036a
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-25 17:57:51 +0530

    Time: 28 ms (27.50%) | Memory: 37.6 MB (50.21%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -8,35 +8,41 @@ public:
         vector<vector<int>> Vis(n,vector<int>(m,0));
         vector<vector<int>> dis(n,vector<int>(m,-1));
 
-        queue<pair<pair<int,int>,int>> q; // here it will capture the indexes as well as the distance 
+        // queue<pair<pair<int,int>,int>> q; // here it will capture the indexes as well as the distance 
+
+        queue<pair<int,int>> q;
 
         for(int i =0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(mat[i][j]== 0){
                     Vis[i][j] = 1;
                     dis[i][j] = 0;
-                    q.push({{i,j},0});
+                    // q.push({{i,j},0});
+                    q.push({i,j});
                 }
             }
         }
 
         vector<int> drow = {-1,0,1,0};
         vector<int> dcol = {0,1,0,-1};
+        int d = 0;
 
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int d = q.front().second;
+            // int r = q.front().first.first;
+            // int c = q.front().first.second;
+            // int d = q.front().second;
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
-
-            dis[r][c] = d;
             for(int i=0;i<4;i++){
                 int nr = r + drow[i];
                 int nc = c + dcol[i];
 
                 if(nr>=0 && nr<n && nc>=0 && nc<m && Vis[nr][nc]==0 ){
                     Vis[nr][nc] = 1;
-                    q.push({{nr,nc},d+1});
+                    // q.push({{nr,nc},d+1});
+                    dis[nr][nc] = dis[r][c] + 1;
+                    q.push({nr,nc});
                 }
             }
         }
