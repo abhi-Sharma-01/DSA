f74b1c98238d2216169046d1e6f0548b9e9d9175
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-26 19:44:22 +0530

    Time: 207 ms (6.54%) | Memory: 57.4 MB (5.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    void dfs(int r,int c , vector<vector<int>>&grid, vector<vector<int>>&Vis){
+        Vis[r][c] = 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> drow = {-1,0,1,0};
+        vector<int> dcol = {0,1,0,-1};
+
+        for(int i=0;i<4;i++){
+            int nr = r + drow[i];
+            int nc = c + dcol[i];
+
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !Vis[nr][nc]){
+                dfs(nr,nc,grid,Vis);
+            }
+        }
+
+    }
     int numEnclaves(vector<vector<int>>& grid) {
         // here our task is to convert the 1 into zero with are a t boundary and lionked to it 
         int n = grid.size();
@@ -11,46 +28,50 @@ public:
         for(int j=0; j<m;j++){
             // capture land fron the top row 
             if(grid[0][j] == 1 && !Vis[0][j]){
-                q.push({0,j});
-                Vis[0][j] = 1;
+                dfs(0,j,grid,Vis); // use -DFS
+                // q.push({0,j});  // for bfs
+                // Vis[0][j] = 1;
             }
             // capture land at the bottom row
             if(grid[n-1][j] == 1 && !Vis[n-1][j]){
-                q.push({n-1,j});
-                Vis[n-1][j] = 1;
+                dfs(n-1,j,grid,Vis); // use -DFS
+                // q.push({n-1,j});
+                // Vis[n-1][j] = 1;
             }
         } 
 
         for(int i=0;i<n;i++){
             // capture land at the first column
             if(grid[i][0]==1 && !Vis[i][0]){
-                q.push({i,0});
-                Vis[i][0] = 1;
+                dfs(i,0,grid,Vis); // use -DFS
+                // q.push({i,0});  // uses BFS
+                // Vis[i][0] = 1;
             }
             if(grid[i][m-1]==1 && !Vis[i][m-1]){
-                q.push({i,m-1});
-                Vis[i][m-1] = 1;
+                dfs(i,m-1,grid,Vis); // use -DFS
+                // q.push({i,m-1});// Use BFS
+                // Vis[i][m-1] = 1;
             }
         }
 
-        vector<int> drow = {-1,0,1,0};
-        vector<int> dcol = {0,1,0,-1};
+        // vector<int> drow = {-1,0,1,0};
+        // vector<int> dcol = {0,1,0,-1};
 
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
+        // while(!q.empty()){
+        //     int r = q.front().first;
+        //     int c = q.front().second;
+        //     q.pop();
 
-            for(int i=0;i<4;i++){
-                int nr = r + drow[i];
-                int nc = c + dcol[i];
+        //     for(int i=0;i<4;i++){
+        //         int nr = r + drow[i];
+        //         int nc = c + dcol[i];
 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !Vis[nr][nc]){
-                    q.push({nr,nc});
-                    Vis[nr][nc] = 1;
-                }
-            }
-        }
+        //         if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !Vis[nr][nc]){
+        //             q.push({nr,nc});
+        //             Vis[nr][nc] = 1;
+        //         }
+        //     }
+        // }
 
         int count =0;
         for(int i=0;i<n;i++){
