203a0930bd6a7656caffe098453c47a89f98251c
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-26 19:36:11 +0530

    Time: 7 ms (77.01%) | Memory: 32 MB (64.06%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,61 +1,65 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>&Vis, vector<vector<int>> &grid){
-        Vis[r][c] = 1;
+    int numEnclaves(vector<vector<int>>& grid) {
+        // here our task is to convert the 1 into zero with are a t boundary and lionked to it 
         int n = grid.size();
         int m = grid[0].size();
 
+        vector<vector<int>> Vis(n,vector<int> (m,0));
+        queue<pair<int,int>> q;
+
+        for(int j=0; j<m;j++){
+            // capture land fron the top row 
+            if(grid[0][j] == 1 && !Vis[0][j]){
+                q.push({0,j});
+                Vis[0][j] = 1;
+            }
+            // capture land at the bottom row
+            if(grid[n-1][j] == 1 && !Vis[n-1][j]){
+                q.push({n-1,j});
+                Vis[n-1][j] = 1;
+            }
+        } 
+
+        for(int i=0;i<n;i++){
+            // capture land at the first column
+            if(grid[i][0]==1 && !Vis[i][0]){
+                q.push({i,0});
+                Vis[i][0] = 1;
+            }
+            if(grid[i][m-1]==1 && !Vis[i][m-1]){
+                q.push({i,m-1});
+                Vis[i][m-1] = 1;
+            }
+        }
+
         vector<int> drow = {-1,0,1,0};
-        vector<int> dcol = {0,1,0,-1}; 
-        // now checking in the all th four direction to get the all the neighbour 1
-        for(int i=0;i<4;i++){
-            int nr = r + drow[i];
-            int nc = c + dcol[i];
+        vector<int> dcol = {0,1,0,-1};
 
-            if(nr>=0 && nr<n && nc>=0 && nc<m && Vis[nr][nc]==0 && grid[nr][nc]==1){
-                dfs(nr,nc,Vis,grid);
-            }
-        }
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
 
-    }
+            for(int i=0;i<4;i++){
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
 
-    int numEnclaves(vector<vector<int>>& grid) {
-        // My Intiution say we can use the dfs method to solve the problem as we have to find the 1 which are not connected to the boundary and surrounded by the sea level i.e where we can reach 
-
-        int n  = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> Vis(n,vector<int>(m,0));
-
-        // We need to find all the 1 which are at the boundry and using dfs we can reach all the 1's that are connected with them and mark them visited so that we need not to come and visit it again
-        for(int j=0;j<m;j++){
-            if(!Vis[0][j] && grid[0][j]==1){ // checking the very first row of grid
-                dfs(0,j,Vis,grid);
-            }
-
-            if(!Vis[n-1][j] && grid[n-1][j]==1){
-                dfs(n-1,j,Vis,grid);
-            }
-        }
-
-        for(int i=0;i<n;i++){
-            if(!Vis[i][0] && grid[i][0]== 1){
-                dfs(i,0, Vis, grid);
-            }
-            if(!Vis[i][m-1] && grid[i][m-1]){
-                dfs(i,m-1,Vis,grid);
-            }
-        }
-
-        int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(Vis[i][j]==0 && grid[i][j]==1){
-                    count++;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !Vis[nr][nc]){
+                    q.push({nr,nc});
+                    Vis[nr][nc] = 1;
                 }
             }
         }
 
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j]==1 && !Vis[i][j]){
+                    count++;
+                }
+            }
+        }
         return count;        
     }
 };
