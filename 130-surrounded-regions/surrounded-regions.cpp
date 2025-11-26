ab775f60acb1fa69f59b10427cc0f95ce3959b44
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-26 18:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (21.39%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,65 +1,70 @@
 class Solution {
 public:
+    void solve(vector<vector<char>>& board) {
+     //  Method 2: Using the BFS approach 
+    // storing all the 'O' into the queue
+    // then mark them visited 
 
-    void dfs(int row,int col, vector<vector<char>> &board , vector<vector<int>> &Vis){
-        int m  = board.size();
-        int n = board[0].size();
+    int m = board.size();
+    int n = board[0].size();
 
-        Vis[row][col] = 1;
+    vector<vector<int> >  Vis(m,vector<int> (n,0));
 
-        vector<int> drow = {-1,0,1,0};
-        vector<int> dcol = {0,1,0,-1};
+    queue<pair<int,int>> q; // it will capture all the 'O' cell which further will be used 
+    
+    for(int j = 0;j<n;j++){
+        //first row
+        if(board[0][j]=='O' && Vis[0][j]==0){
+            q.push({0,j});
+            Vis[0][j] = 1;
+        }
+        if(board[m-1][j]=='O' && Vis[m-1][j]==0){
+            q.push({m-1,j});
+            Vis[m-1][j] = 1;
+        }
+    }
 
-        for(int i =0;i<4 ;i++){
+    // checking the first column and the last columns 
 
-            int nr = row + drow[i];
-            int nc = col + dcol[i];
+    for(int i=0;i<m;i++){
+        //first row
+        if(board[i][0]=='O' && Vis[i][0]==0){
+            q.push({i,0});
+            Vis[i][0] = 1;
+        }
+        if(board[i][n-1]=='O' && Vis[i][n-1] == 0){
+            q.push({i,n-1});
+            Vis[i][n-1] = 1;
+        }
+    }
 
-            if(nr>=0 && nr<m && nc>=0 && nc< n && board[nr][nc]=='O' && Vis[nr][nc]==0){
-                dfs(nr,nc, board,Vis);
+    // Now here we need to apply the BFS Approach and find all the connecte dcell with the "O"
+
+    vector<int> drow = {-1,0,1,0};
+    vector<int> dcol = {0,1,0,-1};
+    while(!q.empty()){
+        int r = q.front().first;
+        int c = q.front().second;
+        q.pop();
+
+        for(int i =0;i<4;i++){
+            int nr = r + drow[i];
+            int nc = c + dcol[i];
+
+            if(nr>=0 && nr<m && nc>=0 && nc<n && Vis[nr][nc]==0 && board[nr][nc]== 'O'){
+                q.push({nr,nc});
+                Vis[nr][nc] = 1;
             }
         }
     }
-    void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
 
-        vector<vector<int>> Vis(m,vector<int> (n,0));
-        // so our goal is to capture all the 'O' at the boundry
-
-        // to check at the boundary we need to capture all the region connected to it with 'O' using DFS we can get all the points that are somehow connected with the edge 'O'
-
-        //  Checking the first row and the last row for all the columns 
-       
-       for(int j = 0;j<n;j++){
-            //first row
-            if(board[0][j]=='O' && Vis[0][j]==0){
-                dfs(0,j,board, Vis);
+    for(int i =0;i<m;i++){
+        for(int j =0;j<n;j++){
+            if(Vis[i][j]== 0 && board[i][j]=='O'){
+                board[i][j] = 'X';
             }
-            if(board[m-1][j]=='O' && Vis[m-1][j]==0){
-                dfs(m-1,j,board, Vis);
-            }
-       }
-
-       // checking the first column and the last columns 
-
-       for(int i=0;i<m;i++){
-            //first row
-            if(board[i][0]=='O' && Vis[i][0]==0){
-                dfs(i,0,board, Vis);
-            }
-            if(board[i][n-1]=='O' && Vis[i][n-1] == 0){
-                dfs(i,n-1,board, Vis);
-            }
-       }
-
-       for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(Vis[i][j]== 0 && board[i][j]=='O'){
-                    board[i][j] = 'X';
-                }
-            }
-       }      
-
+        }
+    }   
+    
     }
 };
