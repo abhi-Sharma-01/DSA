edf1e8cca46cb1195e79ef4cb661a8d6f1298e09
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-27 16:29:57 +0530

    Time: 5 ms (57.71%) | Memory: 20.2 MB (22.44%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
+    bool check_dfs(int node, vector<vector<int>>&adjList,vector<int>&Vis,vector<int> &pathVis){
+        Vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(auto nbr: adjList[node]){
+            if(!Vis[nbr]){
+                if(check_dfs(nbr, adjList, Vis, pathVis)) return true;
+            }else if(pathVis[nbr]){
+                return true;
+            }
+        }
+
+        pathVis[node] = 0; // we need to unvisit the node 
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V = numCourses;
-        vector<vector<int>> adjList(V);
-
-        for(auto edge:prerequisites){
-            adjList[edge[0]].push_back(edge[1]);
-        }
-
-        vector<int> inedge(V);
-        for(int i=0;i<V;i++){
-            for(auto it:adjList[i]){
-                inedge[it]++;
-            }
-        }
-        queue<int> q;
-        for(int i=0;i<V;i++){
-            if(inedge[i]==0){
-                q.push(i);
-            }
-        }
-        vector<int> topo;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for(auto it:adjList[node]){
-                inedge[it]--;
-                if(inedge[it]==0){
-                    q.push(it);
-                }
-            }
         
+        int V = numCourses;
+        if(prerequisites.size()==0) return true;
+        vector<vector<int>> adjList(V);
+        for(auto edge:prerequisites){
+            adjList[edge[1]].push_back(edge[0]);
+        }
+        vector<int> Vis(V,0);
+        vector<int> pathVis(V,0);
+
+
+        for(int i=0;i<V; i++){
+            if(!Vis[i]){
+                if(check_dfs(i,adjList,Vis,pathVis)) return false;
+            }
         }
 
-        if(topo.size()<V){
-            return false;
-        }
-
-        return true;
+        return true;// i.e No cycle found , so we can complete the courses without any deadlock        
     }
 };
