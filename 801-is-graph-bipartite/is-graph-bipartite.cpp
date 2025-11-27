54e0b47a3e9f0d0d694d27fd0e21a65d55eb83e1
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-11-27 13:06:42 +0530

    Time: 2 ms (17.38%) | Memory: 17.5 MB (54.52%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,67 +1,33 @@
 class Solution {
 public:
-    // bool check_bfs (int start, vector<vector<int>>&graph,vector<int> &color){
-    //     // This is the BFS approach of question where we check using the adjancent node of each node and putting it into the queue
-    //     queue<int> q;
-    //     q.push(start);
-    //     color[start] = 0;
-    //     while(!q.empty()){
-    //         int node = q.front();
-    //         q.pop();
+    bool check_bfs(int first,vector<vector<int>>&graph, vector<int> &color){
+        queue<int> q;
+        q.push(first);
+        color[first] = 0;
 
-    //         for(auto it: graph[node]){
-    //             if(color[it]== -1){
-    //                 color[it] = !color[node];
-    //                 q.push(it);
-    //             }else if (color[it]== color[node]){
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
+        while(!q.empty()){
+            int  node = q.front();
+            q.pop();
 
-    // }
-
-    bool check_dfs(int start, int col , vector<vector<int>> &graph, vector<int> &color){
-        color[start] = col;
-        for(auto it: graph[start]){
-            if(color[it]== -1){
-                // col = !col; // we can directly write it in the function
-                if(check_dfs(it, !col,graph,color)== false){
+            for(int nbr : graph[node]){
+                if(color[nbr] == -1){
+                    color[nbr] = !color[node];
+                    q.push(nbr);
+                }else if(color[nbr] == color[node]){ // i.e already colored hai
                     return false;
-                }
-            }else if(color[it] == color[start]){
-                return false;
+                } 
             }
         }
         return true;
     }
-
     bool isBipartite(vector<vector<int>>& graph) {
-
-
-        int V= graph.size();        
-        vector<int> color(V+1, -1);
-
-        // This for the  BFS approach 
-
-        // for(int i=0;i<V;i++){  // we can directly do checking in this function but to chekc the connected component we need to check for each component 
-        //     if(color[i]== -1){
-        //         if(check_bfs(i,graph,color)== false){
-        //             return false;
-        //         }
-        //     }
-        // }
-
-        // Now Below is the code for the current DFS Approach
-
-        for(int i=0;i<V;i++){
-            if(color[i]== -1){
-                if(color[i]==-1){
-                    if(check_dfs(i,0,graph,color) == false){
-                        return false;
-                    }
-                }
+        // we are having an adjList -> graph
+        // Now we need to find the if we apply color on graph no two adjacent should be of same color
+        int V = graph.size();
+        vector<int> color(V,-1);
+        for(int i =0;i<V;i++){
+            if(color[i]== -1){ // starting from the very first node we will traverse the adjacent node and color them just opposite to the node 
+                if(!check_bfs(i,graph,color)) return false;
             }
         }
 
