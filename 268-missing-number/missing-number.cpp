4a972c80ad61a68914faad832ff6467c57141943
abhi-Sharma-01 <abhisharmaabhishek2801@gmail.com>
2025-12-07 13:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.51%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,16 +2,8 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        vector<int> map(n + 1, 0);
-        for(int i=0;i<n;i++){
-            map[nums[i]]=1;
-        }
-        int indx=-1;
-        for(int i=0;i<=n;i++){
-            if(map[i]==0){
-                return i;
-            }
-        }      
-        return -1;
+        int MaxSum = (n*(n+1))/2;
+
+        return MaxSum - accumulate(nums.begin(),nums.end(),0);
     }
 };
